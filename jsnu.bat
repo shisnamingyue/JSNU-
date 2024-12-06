d0e6e106abe3e22005d0e239e77c22d9b7d64fb3
shisnamingyue <164749991+shisnamingyue@users.noreply.github.com>
2024-12-06 17:31:26 +0800

    Update jsnunetwork1.1.bat
    脚本在连接网络后弹出登录界面而不是直接完成登录
    1. 登录URL的问题
    1.0设置的 loginURL 是校园网登录成功后的跳转页面 (success.jsp)，但实际需要的是处理登录信息的接口地址。登录请求没有发送到正确的接口，登录不会生效。
    
    解决方案:
    找到正确的登录接口 URL，例如类似 http://<网关IP>/drcom/login 或 http://<网关IP>/eportal/ 的地址。

--- jsnu.bat
+++ jsnu.bat
@@ -1,6 +1,6 @@
 @echo off
 :: 设置Wi-Fi名称
-set wifiName=i-Star
+set wifiName=
 
 :: 设置校园网账号
 set userId=
@@ -11,7 +11,11 @@ set Password=
 :: 设置目标IP
 set targetIP=
 
-:: 设置登录URL
+:: 设置登录界面URL 类似 http://<网关IP>/drcom/login 或 http://<网关IP>/eportal/ 的地址
+set loginPageURL=
+
+:: 设置登录URL 类似 http://<网关IP>/drcom/login 或 http://<网关IP>/eportal/success 的地址
+
 set loginURL=
 
 :: 测试网络连接
@@ -32,3 +36,8 @@ if %ERRORLEVEL% neq 0 goto CHECK_CONNECTION
 @echo on
 :: 发送登录请求
 curl --max-time 2 --retry 4 --retry-delay 1 -d "DDDDD=%userId%&upass=%Password%" %loginURL%
+
+@echo off
+:: 提示登录完成
+echo 已尝试登录校园网
+pause
