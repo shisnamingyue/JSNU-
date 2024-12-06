5f4d0d5c959a2fcf2840052a74ab7b8c028fd280
shisnamingyue <164749991+shisnamingyue@users.noreply.github.com>
2024-12-06 17:40:51 +0800

    Update network 1.1.bat

--- network 1.1.bat
+++ network 1.1.bat
@@ -3,19 +3,19 @@
 set wifiName=i-Star
 
 :: 设置校园网账号
-set userId=3020235103
+set userId=
 
 :: 设置校园网密码
-set Password=ZZj20050114.
+set Password=
 
 :: 设置目标IP
-set targetIP=202.195.64.98
+set targetIP=
 
-:: 设置登录界面URL
-set loginPageURL=http://202.195.64.98/eportal/index.jsp?wlanuserip=eeca0934a929967b3a9515283a5df31e&wlanacname=f79292c779ba613022cf572f09d96928&ssid=&nasip=35f11b1e8ffa899cbf81061ff6ad8dc4&mac=e263998cb73093f14e44191433461654&t=wireless-v2&url=fbf9194c3bd98c85273eedd6145f126601062e1baff13291&ticket=ST-1284601-Z8fbzcl127owulTeT2cJ94qUu6Mrg-sso-7f4fc7dc9f-w4gxt
+:: 设置登录界面URL  设置登录界面URL 类似 http://<网关IP>/drcom/login 或 http://<网关IP>/eportal/ 的地址
+set loginPageURL=
 
-:: 设置登录URL
-set loginURL=http://202.195.64.98/eportal/success.jsp?userIndex=33356631316231653866666138393963626638313036316666366164386463345f31302e31392e35382e3134365f33303230323335313033&keepaliveInterval=0
+:: 设置登录URL类似 http://<网关IP>/drcom/login 或 http://<网关IP>/eportal/ 的地址
+set loginURL=
 
 :: 测试网络连接
 ping -n 1 -w 100 %targetIP% | findstr /i "TTL" >nul
